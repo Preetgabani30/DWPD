363cd96e5fbbf467133184a206bbc2597c5e8ef1
Preetgabani30 <109951320+Preetgabani30@users.noreply.github.com>
2022-09-29 14:23:57 +0530

    Add files via upload

--- Extra8.php
+++ Extra8.php
@@ -1,8 +1,8 @@
 <?php 
-	$uername = $_POST['Username']; 
+	$name = $_POST['Username']; 
 	$age = $_POST['Age'];  
-	setcookie('$uname',$uername,time() + 3600); 
+	setcookie('$uname',$name,time() + 3600); 
 	setcookie('$age',$age,time() + 3600);  
-	echo $_COOKIE['uername']."<br>"; 
-	echo $_COOKIE['age']; 
+	echo $_COOKIE['$uname']."<br>"; 
+	echo $_COOKIE['$age']; 
 ?> 
